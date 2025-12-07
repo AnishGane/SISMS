961cdca52853a8602da458d892379109b5845ae1
Anish Gane <anishgane10@gmail.com>
2025-12-07 19:13:41 +0545

    Enhance product management and search functionality
    - Extend product controller to include user authentication checks for creating, updating, and fetching products.
    - Implement search functionality with hybrid scoring for product names, including substring matching and fuzzy search.
    - Update product routes to support new endpoints for fetching and searching products.
    - Remove deprecated search route file to streamline codebase.
    - Add a new MonthlySalesChart component to the admin dashboard for improved sales insights.

--- backend/src/routes/shared/products.route.ts
+++ backend/src/routes/shared/products.route.ts
@@ -1,10 +1,27 @@
 import express from "express";
-import { createProduct } from "../../controller/shared/product.controller.js";
+import {
+  createProduct,
+  getEachProduct,
+  getProducts,
+  updateProduct,
+} from "../../controller/shared/product.controller.js";
 import { allowRoles } from "../../middlewares/role.middleware.js";
 import { auth } from "../../middlewares/auth.middleware.js";
+import { searchProducts } from "../../controller/shared/search.controller.js";
 
 const productRoutes = express.Router();
 
 productRoutes.post("/add-product", auth, allowRoles("admin"), createProduct);
+
+// GET all products with pagination, filters, sorting
+productRoutes.get("/", auth, getProducts);
+
+productRoutes.get("/:id", auth, getEachProduct);
+
+//PUT /api/products/update/6932eea77a9e7f7e1e427952
+productRoutes.put("/update/:id", auth, updateProduct);
+
+// GET /api/products/search?search=notebook
+productRoutes.get("/search", auth, searchProducts);
 
 export default productRoutes;
